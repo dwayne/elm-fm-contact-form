e0603bb314e0d99897c821c431866c251e4abb19
Dwayne Crooks <me@dwaynecrooks.com>
2025-08-28 04:49:02 -0400

    Improve Sass module resolution with loadPaths

--- workshop/astro.config.mjs
+++ workshop/astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import path from 'node:path';
+import url from 'node:url';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          loadPaths: [
+            path.resolve(__dirname, 'src/styles')
+          ]
+        }
+      }
+    }
+  }
+});
