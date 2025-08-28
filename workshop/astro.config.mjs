b76d360a98087cbf389e5b40f376af0f1e4b66f6
Dwayne Crooks <me@dwaynecrooks.com>
2025-08-28 06:34:35 -0400

    Respect Astro's base configuration in Sass

--- workshop/astro.config.mjs
+++ workshop/astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 import path from 'node:path';
+import process from 'node:process';
 import url from 'node:url';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const base = process.env.ASTRO_BASE || '/';
 
 // https://astro.build/config
 export default defineConfig({
+  base,
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
+          additionalData: `$base: "${base}";`,
           loadPaths: [
             path.resolve(__dirname, 'src/styles')
           ]
