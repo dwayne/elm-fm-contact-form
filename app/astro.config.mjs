5dc4bd4f47fecd9c7176f374972fed6230edf673
Dwayne Crooks <me@dwaynecrooks.com>
2025-09-09 06:59:15 -0400

    Start working on app

---
+++ app/astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import elmPlugin from "vite-plugin-elm";
+import process from 'node:process';
+
+const base = process.env.ASTRO_BASE || '/';
+
+// https://astro.build/config
+export default defineConfig({
+  base,
+  vite: {
+    plugins: [
+      elmPlugin()
+    ]
+  }
+});
